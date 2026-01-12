2a507005f83388c64b163a841fb0691109fbdcf2
Himanshu Gohil <himanshugohil828@gmail.com>
2026-01-12 22:52:51 +0530

    Create QUESTION-19.C

---
+++ C PROGRAMS/QUESTION-19.C
@@ -0,0 +1,20 @@
+/*
+Write a C program to print all natural numbers in reverse (from n to 1)
+using while loop.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n >= 1) {
+        printf("%d ", n);
+        n--;
+    }
+
+    return 0;
+}
