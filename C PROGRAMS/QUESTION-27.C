c63ee0a522e6edd2134869724b0ee6925a245789
Himanshu Gohil <himanshugohil828@gmail.com>
2026-01-12 23:02:46 +0530

    Create QUESTION-27.C

---
+++ C PROGRAMS/QUESTION-27.C
@@ -0,0 +1,12 @@
+/*
+27. Write a program to print student’s name, address, contact number.
+*/
+#include <stdio.h>
+
+int main() {
+    printf("Name: Rahul\n");
+    printf("Address: Ahmedabad\n");
+    printf("Contact: 9876543210");
+
+    return 0;
+}
