360b20db1fb9f9f3ebf2336e7d30e9ed3f3160be
Himanshu Gohil <himanshugohil828@gmail.com>
2026-01-12 22:52:37 +0530

    Add program to print numbers from 1 to 10
    Implement a program to print numbers from 1 to 10 using a for loop.

---
+++ C PROGRAMS/QUESTION-18.C
@@ -0,0 +1,15 @@
+/*
+Program to print numbers from 1 to 10 using for loop
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 1; i <= 10; i++) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
