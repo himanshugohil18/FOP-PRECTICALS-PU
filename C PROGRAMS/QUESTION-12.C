11fbb2dd991c36028616d35204906e3cfc48c0cc
Himanshu Gohil <himanshugohil828@gmail.com>
2026-01-12 22:49:06 +0530

    Create QUESTION-12.C

---
+++ C PROGRAMS/QUESTION-12.C
@@ -0,0 +1,31 @@
+/*
+Write a C program to check whether a string is palindrome.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, length, flag = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    length = strlen(str);
+
+    for (i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - i - 1]) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0) {
+        printf("The string is a Palindrome");
+    } else {
+        printf("The string is not a Palindrome");
+    }
+
+    return 0;
+}
