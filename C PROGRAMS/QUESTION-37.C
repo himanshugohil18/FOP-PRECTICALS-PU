417dd2cb7e3627798a249acc10530d25d11a80ff
Himanshu Gohil <himanshugohil828@gmail.com>
2026-01-12 23:05:39 +0530

    Create QUESTION-37.C

---
+++ C PROGRAMS/QUESTION-37.C
@@ -0,0 +1,21 @@
+/*
+37. Add two numbers using Binary Operator Overloading.
+*/
+#include <iostream>
+using namespace std;
+
+class Test {
+public:
+    int x;
+    Test(int a) { x = a; }
+    Test operator+(Test t) {
+        return Test(x + t.x);
+    }
+};
+
+int main() {
+    Test t1(10), t2(20);
+    Test t3 = t1 + t2;
+    cout << t3.x;
+    return 0;
+}
